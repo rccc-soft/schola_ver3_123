2ac880de7c5629dc56efd58f1c812711804b4eec
rccc-soft <rccc7597@gmail.com>
2025-01-23 01:13:34 +0900

    和田　build.gradle.kts の更新

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,4 +48,7 @@ dependencies {
     androidTestImplementation(libs.espresso.core)
     implementation ("com.sun.mail:android-mail:1.6.7")
     implementation ("com.sun.mail:android-activation:1.6.7")
+
+    implementation("com.google.zxing:core:3.5.2")
+    implementation("com.journeyapps:zxing-android-embedded:4.3.0")
 }
